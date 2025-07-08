8867e0878055c5df5ba7c765a15e8d539c57f53d
chavesjayemerald <chaves.jayemerald@gmail.com>
2025-07-08 21:04:00 +0800

    Seventh commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <nav>
-
+      <nav className="social-links">
+        <a href="https://www.linkedin.com/in/chavesjayemerald/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a href="https://www.facebook.com/ymeraldchaves/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://github.com/chavesjayemerald" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
       </nav>
     </footer>
   );
