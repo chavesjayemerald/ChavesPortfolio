9e601688804dc0564be10f94b3f6da8d13bc307d
chavesjayemerald <chaves.jayemerald@gmail.com>
2025-07-08 19:49:07 +0800

    Fifth commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <section id="home" className="section home-section">
       <h1>Welcome to My Portfolio</h1>
       <p>This is the introduction.</p>
-      <img src="./public/images/profile.jpg" alt="Profile" className="profile-img" />
+      <img src="./images/profile.jpg" alt="Profile" className="profile-img" />
     </section>
   );
 }
