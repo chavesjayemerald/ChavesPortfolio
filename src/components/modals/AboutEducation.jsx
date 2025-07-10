6837fb257cde10c945a5f937f820ac670f0f4811
chavesjayemerald <chaves.jayemerald@gmail.com>
2025-07-10 17:01:07 +0800

    Eleventh commit

--- src/components/modals/AboutEducation.jsx
+++ src/components/modals/AboutEducation.jsx
@@ -2,10 +2,24 @@ export default function AboutEducation({ onClose }) {
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <h2>Education</h2>
-        <ul>
-          <li><strong>Education:</strong> Bachelor of Science in Information Techonlogy</li>
-        </ul>
+        <h2>COLLEGE</h2>
+        <div className="modal-content">
+          <img src="./images/example.jpg" alt="Example" className="education-img" />
+          <p><strong>Year Started:</strong><br />2021</p>
+          <p><strong>Year Graduated:</strong><br />2025</p>
+          <p><strong>Course/Program:</strong><br />Bachelor of Science in Information Technology</p>
+          <p><strong>University:</strong><br />University of Science and Technology of Southern de Oro Philippines College</p>
+        </div>
+
+
+        <h2>COLLEGE</h2>
+        <div className="modal-content">
+          <img src="./images/example.jpg" alt="Example" className="education-img" />
+          <p><strong>Year Started:</strong><br />2021</p>
+          <p><strong>Year Graduated:</strong><br />2025</p>
+          <p><strong>Course/Program:</strong><br />Bachelor of Science in Information Technology</p>
+          <p><strong>University:</strong><br />University of Science and Technology of Southern de Oro Philippines College</p>
+        </div>
         <button onClick={onClose} className="modal-close-btn">Close</button>
       </div>
     </div>
